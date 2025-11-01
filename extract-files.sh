bf7835a8b660987a1edeed9028592800131cce57
vannxavero <vannxaveroo@proton.me>
2025-11-01 16:47:42 +0000

    S666LN: Change vndk of libstagefright_foundation-v33 to v32
    VNDK 33 is not available on Android 13, so we switched to VNDK 22.
    
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -66,7 +66,7 @@ function blob_fixup() {
             ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
             "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
-            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v32.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.gnss-service.mediatek |\
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
