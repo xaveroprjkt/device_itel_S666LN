ad33f319767d92d4aef32ecc82ec948558aadfb3
vannxavero <vannxaveroo@proton.me>
2025-11-01 15:14:47 +0000

    S666LN: Move To OSS Services Thermal
    Thermal Mediatek common services is not avaliable at lineage-20 so we change it to oss
    
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- device.mk
+++ device.mk
@@ -472,7 +472,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.mediatek
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
