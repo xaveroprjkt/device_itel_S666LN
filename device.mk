bc71b9206e0ed12cdbb3d48446adc4b9296ef6d2
vannxavero <vannxaveroo@proton.me>
2025-11-01 15:05:49 +0000

    S666LN: Change Health Services
    common health services is not avaliable at Android 13
    
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- device.mk
+++ device.mk
@@ -200,8 +200,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.mediatek \
-    android.hardware.health-service.mediatek-recovery \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
     charger_res_images_vendor
     
 # HIDL
