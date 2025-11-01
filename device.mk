03228a3164be34276a8edcbaca2876bfb66780b0
vannxavero <vannxaveroo@proton.me>
2025-11-01 15:01:33 +0000

    S666LN: Switch Using Boot 1.2 services
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- device.mk
+++ device.mk
@@ -99,8 +99,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    com.android.hardware.boot \
-    android.hardware.boot-service.default_recovery
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery
 
 # Camera
 PRODUCT_PACKAGES += \
