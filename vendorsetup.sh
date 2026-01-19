d673156281e9408681e85bddb6a0eda276c3c55b
vannkyle <akunvng58@gmail.com>
2026-01-19 11:14:53 +0700

    S666LN: track some repo's to own org.
    Signed-off-by: vannkyle <akunvng58@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,10 +2,10 @@
 git clone -b lineage-21 https://github.com/dns24/kernel_itel_S666LN device/itel/S666LN-kernel
 
 # Clone Device Vendor Tree
-git clone -b lineage-21 https://github.com/dns24/vendor_itel_S666LN vendor/itel/S666LN
+git clone -b lineage-20 https://github.com/xaveroprjkt/vendor_itel_S666LN vendor/itel/S666LN
 
 # Clone Device Hardware
-git clone -b lineage-20.0 https://github.com/Anything-at-25-00/hardware_millennium hardware/transsion
+git clone -b lineage-20.0 https://github.com/xaveroprjkt/hardware_transsion hardware/transsion
 
 # Clone Device Hardware Mediatek
 rm -rf hardware/mediatek
