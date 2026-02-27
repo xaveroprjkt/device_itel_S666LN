e043dd86f391bef085ee600d1c3d3170a035b350
vannkyle <akunvng58@gmail.com>
2026-02-27 10:49:03 +0000

    S666LN: Drop prebuilt GoogleCameraGo
    This reverts commit 2397a0c90a5aa1bb4a0663dbd7dfc6b7d06f413c.

--- device.mk
+++ device.mk
@@ -170,15 +170,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-# Google camera
-PRODUCT_PACKAGES += \
-    GoogleCameraGo
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/GoogleCameraGo/permissions/com.google.android.GoogleCameraGo.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCameraGo.xml \
-    $(LOCAL_PATH)/GoogleCameraGo/configs/hiddenapi-package-whitelist-GoogleCameraGo.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-GoogleCameraGo.xml
-
-# Keymaster
 PRODUCT_PACKAGES += \
     libgatekeeper.vendor
 
